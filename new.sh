4719182b04991a9b300222907a9adf91d59d433b
marid00 <d.maris@belastingdienst.nl>
2022-12-01 10:17:14 +0100

    Update new script

--- new.sh
+++ new.sh
@@ -2,11 +2,16 @@
 echo What day is it? 
 read day 
 
+# Add the testfiles
+touch input/$day-text.txt
+touch input/$day.txt
+
 # Create a new folder with the day name
 cd src 
 mkdir $day 
 
 # Create new files for part1 and part2
 cd $day 
+touch part1.ts part2.ts assignment.txt
 echo 'import { readFileSync } from "fs";\n\nexport function getInput(file: string){\nreturn readFileSync(file, "utf-8");\n}\n\nexport function part1(file: string) {\nconst input = getInput(file);\nconsole.log(input);\n\nreturn 0;\n}' > part1.ts
 echo 'import { getInput } from "./part1";\n\nexport function part2(file: string) {\nconst input = getInput(file);\nconsole.log(input);\n\nreturn 0;\n}' > part2.ts
