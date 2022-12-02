d552fbe4f84abb38eac88012f1b9583655bd35db
marid00 <d.maris@belastingdienst.nl>
2022-12-02 12:45:45 +0100

    remove assignment files

--- new.sh
+++ new.sh
@@ -12,6 +12,5 @@ mkdir $day
 
 # Create new files for part1 and part2
 cd $day 
-touch part1.ts part2.ts assignment.txt
 echo 'import { readFileSync } from "fs";\n\nexport function getInput(file: string){\nreturn readFileSync(file, "utf-8");\n}\n\nexport function part1(file: string) {\nconst input = getInput(file);\nconsole.log(input);\n\nreturn 0;\n}' > part1.ts
 echo 'import { getInput } from "./part1";\n\nexport function part2(file: string) {\nconst input = getInput(file);\nconsole.log(input);\n\nreturn 0;\n}' > part2.ts
